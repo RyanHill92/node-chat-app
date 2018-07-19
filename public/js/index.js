83ad50f3f7429457af858be4c6eade73927f145a
Ryan Hill <ryanchill319@gmail.com>
2018-07-19 16:36:07 -0400

    Used moment to clean up time stamps

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,20 @@ socket.on('connect', () => {
 });
 
 socket.on('newMessage', (message) => {
+  let time = moment(message.createdAt).format('h:mm a');
   let li = document.createElement('li');
-  li.innerHTML = `${message.from}: ${message.text}`;
+  li.innerHTML = message.from + ', ' + '<em>' + time + '</em>' + ': ' + message.text;
   document.querySelector('#messages').appendChild(li);
 });
 
 socket.on('newLocationMessage', function(locationMessage) {
+  let time = moment(locationMessage.createdAt).format('h:mm a');
   let li = document.createElement('li');
   let a = document.createElement('a');
   a.setAttribute('target', '_blank');
   a.setAttribute('href', locationMessage.url);
-  a.innerHTML = 'here';
-  li.innerHTML = `${locationMessage.from} is `;
+  li.innerHTML = locationMessage.from + ', ' + '<em>' + time + '</em>' + ': Check out my current ';
+  a.innerHTML = 'location.';
   li.appendChild(a);
   document.querySelector('#messages').appendChild(li);
 });
@@ -33,15 +35,18 @@ var messageForm = document.getElementById('message-form');
 //The e (event) argument is crucial for overriding default behavior.
 //It seems that 'submit' is a built-in event for forms. Nice.
 //Notice here, because it's a form, we have 'submit,' not 'click.'
+
+var messageTextBox = document.querySelector('input[name=message]');
+
 messageForm.addEventListener('submit', function (e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
     //Name of element, with attribute and value inside brackets.
     //It seems '.value' is a property of the element. Cool!
-    text: document.querySelector('input[name=message]').value
+    text: messageTextBox.value
   }, function (data) {
-    console.log(data);
+    messageTextBox.value = '';
   });
 });
 
@@ -51,9 +56,15 @@ locationButton.addEventListener('click', function () {
     return alert('Geolocation unavailable on current browser.');
   }
 
+  locationButton.setAttribute('disabled', 'disabled');
+  locationButton.innerHTML = 'Sending location...'
+
   navigator.geolocation.getCurrentPosition(function (position) {
     socket.emit('createLocationMessage', {latitude: position.coords.latitude, longitude: position.coords.longitude});
+    locationButton.innerHTML = 'Send location'
+    locationButton.removeAttribute('disabled');
   }, function () {
     return alert('Unable to share location.');
+    locationButton.removeAttribute('disabled');
   });
 });
